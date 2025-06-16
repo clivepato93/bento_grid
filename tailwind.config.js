9c9fab5db8ce146db6fa25cdb5ed3b4e7f04d425
clive pato <clivepato@clives-MacBook-Pro.local>
2025-06-16 21:39:30 +0100

    font family started

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    //.... All the initial code
+    theme: {
+        extend: {
+            fontFamily: {
+                'DMSans' : ['DMSans', 'sans-serif'],  // after you font, add some fonts separated by commas to handle fallback.
+            }
+        }
+    }
+    //.... All the initial code
+}
